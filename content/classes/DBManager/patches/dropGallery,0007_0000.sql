f1699047d7c48a81f7038cdbe25f55c098eaf3a8
AkBKukU <shelby@jueden.org>
2015-05-11 23:32:05 -0700

    School Project Finished

--- content/classes/DBManager/patches/dropGallery,0007_0000.sql
+++ content/classes/DBManager/patches/dropGallery,0007_0000.sql
@@ -8,6 +8,10 @@ Required: Yes
 
 \*                                                                           */
 
+
+/** add_file
+  * Inserts a new file into the files table
+  */
 DROP PROCEDURE IF EXISTS add_file;
 CREATE PROCEDURE add_file ( inQuickhash VARCHAR(32), inTitle VARCHAR(80) , inDescription VARCHAR(2500), inId_mimetype INTEGER, inFilename VARCHAR(200), inFilesize INTEGER  )
 BEGIN
@@ -18,6 +22,9 @@ INSERT INTO files ( quickhash , title , description , id_mimetype , datetime_add
 END;
 
 
+/** add_mimetype
+  * Inserts a new mimtype
+  */
 DROP PROCEDURE IF EXISTS add_mimetype;
 CREATE PROCEDURE add_mimetype ( inMimetype VARCHAR(150) )
 BEGIN
@@ -27,6 +34,9 @@ INSERT INTO mimetypes(mimetype) VALUES (inMimetype);
 END;
 
 
+/** get_mimetype
+  * Return the mimetype id and name
+  */
 DROP PROCEDURE IF EXISTS get_mimetype;
 CREATE PROCEDURE get_mimetype ( inMimetype VARCHAR(150) )
 BEGIN
@@ -38,6 +48,9 @@ WHERE mimetypes.mimetype = inMimetype;
 END;
 
 
+/** check_file_quickhash
+  * Sends back the same info if it exists
+  */
 DROP PROCEDURE IF EXISTS check_file_quickhash;
 CREATE PROCEDURE check_file_quickhash ( inQuickhash VARCHAR(32) )
 BEGIN
@@ -49,6 +62,9 @@ WHERE files.quickhash = inQuickhash;
 END;
 
 
+/** get_file_basic
+  * Returns basic information about a file
+  */
 DROP PROCEDURE IF EXISTS  get_file_basic;
 CREATE PROCEDURE get_file_basic ( inQuickhash VARCHAR(32) )
 BEGIN
@@ -58,3 +74,21 @@ FROM files JOIN mimetypes USING(id_mimetype)
 WHERE files.quickhash = inQuickhash
 
 END;
+
+
+/** get_file_tags
+  * A subquery to get the tags associated with a file
+  */
+DROP PROCEDURE IF EXISTS  get_file_tags;
+CREATE PROCEDURE get_file_tags ( inQuickhash VARCHAR(32) )
+BEGIN
+
+select distinct files.id_file, files.title , contags.ctags from 
+files join file_tags using(id_file) join 
+(
+	select group_concat(tags.name) as ctags, file_tags.id_file from tags join file_tags using(id_tag) group by id_file
+) as contags on (contags.id_file = file_tags.id_file)
+WHERE files.quickhash = inQuickhash;
+
+END;
+
